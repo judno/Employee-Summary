07ed1b7c128700074d3f574b91a196c780444cbc
udahjay <judahjaimeslatter@gmail.com>
2021-02-10 17:08:47 +1100

    Complete assignment

--- app.js
+++ app.js
@@ -10,8 +10,111 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employees = [];
 
-// Write code to use inquirer to gather information about the development team members,
+async function addEmployee() {
+  const { employeeType } = await inquirer.prompt({
+    name: "employeeType",
+    message: "What type of employee would you like to add?",
+    type: "list",
+    choices: ["Engineer", "Intern", "Manager"],
+  });
+
+  let commonQuestions = [
+    {
+      name: "name",
+      message: "Enter employee's First Name",
+      type: "input",
+    },
+    {
+      name: "id",
+      message: "Enter employee's id",
+      type: "input",
+    },
+    {
+      name: "email",
+      message: "Enter employee's Email",
+      type: "input",
+    },
+  ];
+
+  if (employeeType === "Engineer") {
+    const answers = await inquirer.prompt([
+      ...commonQuestions,
+      {
+        name: "github",
+        message: "What is your Github name?",
+        type: "input",
+      },
+    ]);
+
+    const e = new Engineer(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.github
+    );
+    employees.push(e);
+  } else if (employeeType === "Intern") {
+    const answers = await inquirer.prompt([
+      ...commonQuestions,
+      {
+        name: "school",
+        message: "What School do you go to?",
+        type: "input",
+      },
+    ]);
+
+    const i = new Intern(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.school
+    );
+    employees.push(i);
+  } else {
+    const answers = await inquirer.prompt([
+      ...commonQuestions,
+      {
+        name: "officeNumber",
+        message: "What is your office number?",
+        type: "input",
+      },
+    ]);
+    const m = new Manager(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.officeNumber
+    );
+    employees.push(m);
+  }
+  const { shouldCreateEmployee } = await inquirer.prompt({
+    name: "shouldCreateEmployee",
+    message: "Create new employee?",
+    type: "confirm",
+  });
+
+  return shouldCreateEmployee;
+}
+
+async function run() {
+  let shouldCreateMore = await addEmployee();
+
+  while (shouldCreateMore) {
+    shouldCreateMore = await addEmployee();
+  }
+
+  let renderResults = render(employees);
+
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(outputPath, renderResults);
+}
+
+run();
+
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 // After the user has input all employees desired, call the `render` function (required
